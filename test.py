a25d40f0f80b41485fb3ae189683ac2e8cd7299f
Xuelu Feng <gcctryg@gmail.com>
2021-05-22 23:52:41 -0700

    coverage

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ import leapYear
 
 class testCaseLeapYear(unittest.TestCase):
     def testLeapYear(self):
-        #result = calc.add(10,5)
-       # self.assertEqual(result, 15)
         self.assertTrue(leapYear.checkLeapYear(2003))
         self.assertFalse(leapYear.checkLeapYear(2003))
 
